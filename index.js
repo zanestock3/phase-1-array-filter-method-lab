e7173cef7d3778766e143f7fde595a02f1d2fc07
Zane Stock <zanestock3@gmail.com>
2022-04-12 20:57:51 -0500

    commit tree 1cca4b26d91bd558c4cee322ec69379bc4ca1028

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, query) {
+return drivers.filter(function(driver) {
+    return driver.toLowerCase() === query.toLowerCase();
+}
+)}
+
+function fuzzyMatch(drivers, query) {
+    const lengthOfQuery = query.length
+    return drivers.filter(function(driver) {
+        return driver.slice(0, lengthOfQuery) === query
+    })
+}
+
+function matchName(drivers, query) {
+    const result = drivers.filter(driver => driver.name === query)
+    return result
+}
